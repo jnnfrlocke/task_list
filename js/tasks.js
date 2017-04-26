a631e3d95e8fc6cb85cf21190b3952448b1dfd23
Jen <jnnfrlocke@gmail.com>
2017-04-25 22:04:01 -0500

    Create themes and animation

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,34 @@
 $('li').click(function() {
     $(this).fadeOut('slow');
 })
+
+// Choose new color scheme
+$('#sky_theme').click(
+    function() {
+        $('h1').css('color', '#363732');
+        $('li').css('color', '#363732');
+        $('ul').css('background', '#53D8FB');
+        $('header').css('background', '#DCE1E9');
+    });
+
+$('#nat_theme').click(
+    function() {
+        $('h1').css('color', '#6A7162');
+        $('li').css('color', '#6A7162');
+        $('ul').css('background', '#CBE9D2');
+        $('header').css('background', '#E2DAD0');
+    }
+);
+
+// Animate Header
+$('#fade_out').click(
+    function() {
+        $("header").fadeTo("slow", 0.50);
+    }
+)
+
+$('#fade_in').click(
+    function() {
+        $("header").fadeTo("slow", 1);
+    }
+)
