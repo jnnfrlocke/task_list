940a77ccab52ff5a598da83986899b738660b269
Jen <jnnfrlocke@gmail.com>
2017-04-25 23:05:19 -0500

    Add ability to create new task

--- js/tasks.js
+++ js/tasks.js
@@ -41,12 +41,28 @@ $("#fade").click(function() {
 
 $('#text_up').click(function() {
     $('h1, button, li').css('font-size', function() {
-        return parseInt($(this).css('font-size')) + 5 + 'px';
+        return parseInt($(this).css('font-size')) + 2 + 'px';
     });
 });
 
 $('#text_down').click(function() {
     $('h1, button, li').css('font-size', function() {
-        return parseInt($(this).css('font-size')) - 5 + 'px';
+        return parseInt($(this).css('font-size')) - 2 + 'px';
     });
 });
+
+// Enter New Task
+
+$('#new_task').click(function() {
+    var newTask = prompt("Please Add the new task");
+    if (newTask != null) {
+        $('ul').append('<li>' + newTask + '</li>');
+    }
+});
+
+
+// $('#new_task').click(function() {
+//     $jPrompt('Type something:', 'Prefilled value', 'Prompt Dialog', function(r) {
+//         if (r) jAlert('You entered: ' + r, 'Result');
+//     });
+// })
