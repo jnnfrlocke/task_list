fddae781aa5c40393b00fab45077d01677299c85
Jen <jnnfrlocke@gmail.com>
2017-05-02 20:25:28 -0500

    Create array and display it as a list. Get and display number of items in list.

--- js/tasks.js
+++ js/tasks.js
@@ -1,72 +1,14 @@
-// Choose new color scheme
-// $('#sky_theme').click(
-//     function() {
-//         if ($('section').hasClass("sky_theme")) {
-//             // Show message to user that they switched theme
-//             alert("You have switched to the Sky theme!");
-//         } else {
-//             $('section').css(".skytheme");
-//             $('section').addClass("sky_theme");
-//         }
-//     });
+var shopping = ["Apples", "Hazelnut Milk", "Cherries", "Bananas", "Bread", "Peanut Butter", "Blackberry Jelly", "Frozen Pineapple", "Frozen Strawberries", "Protein Powder", "Spinach", "Cereal", "Waffles", "Ground Beef", "Tortillas"]
 
+var itemCount = shopping.length;
+document.getElementById('itemNum').textContent = "You have " + itemCount + " items on your list.";
 
-// $('#nat_theme').click(function() {
-//     if ($('section').hasClass("sky_theme")) {
-//         $('section').removeClass('sky_theme');
-//         $('section').addClass("nat_theme");
-
-//         // Show message to user that they switched theme
-//         alert("You have switched to the Natural theme!");
-
-//         // $('section').css("class", "nat_theme");
-
-//     }
-// });
-
-var Shopping = ["Apples", "Hazelnut Milk", "Cherries", "Bananas", "Bread", "Peanut Butter", "Blackberry Jelly", "Frozen Pineapple", "Frozen Strawberries", "Protein Powder", "Spinach", "Cereal", "Waffles", "Ground Beef", "Tortillas"]
-
-$('button').click(function() {
-    var thisButton = $(this);
-    // alert(thisButton.attr('id'));
-    if (thisButton.attr('id') == "sky_theme" || thisButton.attr('id') == "nat_theme") {
-        $('section').removeClass('sky_theme nat_theme');
-        $('section').addClass(thisButton.attr('id'));
-    }
-})
-
-
-// Animate Header
-$("#fade").click(function() {
-    if ($('header').hasClass("full")) {
-        $('header').fadeTo("slow", .5);
-        $('header').removeClass("full");
-    } else {
-        $('header').addClass("full");
-        $('header').fadeTo("slow", 1);
-
-    }
-});
-
-// Increase and Decrease Font
-
-$('#text_up').click(function() {
-    $('h1, button, li').css('font-size', function() {
-        return parseInt($(this).css('font-size')) + 2 + 'px';
-    });
-});
-
-$('#text_down').click(function() {
-    $('h1, button, li').css('font-size', function() {
-        return parseInt($(this).css('font-size')) - 2 + 'px';
-    });
-});
-
-// Enter New Task
-
-$('#new_task').click(function() {
-    var newTask = prompt("Please Add the new task"); {
-        // if ($('#sky_theme').hasClass("full"))
-        $('ul').append('<li>' + newTask + '</li>');
-    }
-});
+for (var counter = 0; counter < itemCount; counter++) {
+    // console.log(shopping[count]);
+    var li = document.createElement("li");
+    var text = document.createTextNode(shopping[counter]);
+    // console.log(text);
+    li.appendChild(text);
+    document.getElementById('groceries').appendChild(li);
+    counter++;
+}
