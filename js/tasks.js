7fd9e9dc39f8dd9de4afab1d9053518c26d5738e
Jen <jnnfrlocke@gmail.com>
2017-05-02 21:02:50 -0500

    Create empty array

--- js/tasks.js
+++ js/tasks.js
@@ -1,4 +1,4 @@
-var shopping = ["Apples", "Hazelnut Milk", "Cherries", "Bananas", "Bread", "Peanut Butter", "Blackberry Jelly", "Frozen Pineapple", "Frozen Strawberries", "Protein Powder", "Spinach", "Cereal", "Waffles", "Ground Beef", "Tortillas"]
+var shopping = []
 
 var itemCount = shopping.length;
 document.getElementById('itemNum').textContent = "You have " + itemCount + " items on your list.";
@@ -10,5 +10,14 @@ for (var counter = 0; counter < itemCount; counter++) {
     // console.log(text);
     li.appendChild(text);
     document.getElementById('groceries').appendChild(li);
-    counter++;
 }
+
+// var counter = 0;
+// while (counter < shopping.length) {
+//     var li = document.createElement("li");
+//     var text = document.createTextNode(shopping[counter]);
+//     // console.log(text);
+//     li.appendChild(text);
+//     document.getElementById('groceries').appendChild(li);
+//     counter++;
+// }
