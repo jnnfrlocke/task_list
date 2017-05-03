962ea9b25e8de20405c2ab515ad9d36c641fec18
Jen <jnnfrlocke@gmail.com>
2017-05-02 23:06:38 -0500

    Add functionality

--- js/tasks.js
+++ js/tasks.js
@@ -1,16 +1,46 @@
-var shopping = []
+// Create empty array
+var shopping = [];
+var itemCount;
 
-var itemCount = shopping.length;
-document.getElementById('itemNum').textContent = "You have " + itemCount + " items on your list.";
-
-for (var counter = 0; counter < itemCount; counter++) {
-    // console.log(shopping[count]);
-    var li = document.createElement("li");
-    var text = document.createTextNode(shopping[counter]);
-    // console.log(text);
-    li.appendChild(text);
-    document.getElementById('groceries').appendChild(li);
+// Set item count and display number of items in list
+function getItemCount() {
+    itemCount = shopping.length;
+    document.getElementById('itemNum').textContent = "You have " + itemCount + " items on your list.";
 }
+
+// Show array items in list format 
+function showItems(addLi) {
+    for (var counter = 0; counter < itemCount; counter++) {
+        // console.log(shopping[count]);
+        var li = document.createElement("li");
+        var text = document.createTextNode(addLi[counter]);
+        // console.log(text);
+        li.appendChild(text);
+        document.getElementById('groceries').appendChild(li);
+    }
+}
+
+// Add new item to array
+function addGroceryItemToArray() {
+    // Delete li
+    var groceries = document.getElementById('groceries');
+    groceries.innerHTML = '';
+    // Get new item from input
+    var newItem = document.getElementById('itemsAdded').value;
+    // Add to array
+    shopping.push(newItem);
+    // Update item count for header
+    getItemCount();
+    // Load List
+    showItems(shopping);
+}
+
+// Get Initial Count
+getItemCount();
+// Get Initial Grocery List 
+showItems(shopping);
+
+// Show new items in array on the page
 
 // var counter = 0;
 // while (counter < shopping.length) {
